aea66c3fb8f3f3275495bbc504b76ccdc454025f
jiwei-hemeng <jiwei-hemeng@users.noreply.github.com>
2023-04-21 03:48:20 +0000

    Automated deployment: Fri Apr 21 03:48:20 UTC 2023 09e997810fb78876a39217df12a12a17b74eadb2

---
+++ assets/expenses-64dc8bbf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d98e495c.js";function r(){return e("main",{children:e("h2",{children:"Expenses"})})}export{r as default};
---
+++ assets/invoices-c918eff1.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-d98e495c.js";function i(){return n("main",{children:n("h2",{children:"Invoices"})})}export{i as default};
