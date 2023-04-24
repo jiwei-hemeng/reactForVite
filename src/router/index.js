ca2a5a8b82f2f7f22c4c90c395a92df9c7253d0d
jijiwei <jijiwei@lilianinfo.com>
2023-04-24 14:05:28 +0800

    动态配置路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 // @ts-nocheck
 import { lazy } from "react";
 export const router = {
-  // home: lazy(() => import("@/pages/expenses.jsx")),
   invoices: lazy(() => import("@/pages/invoices.jsx")),
 };
