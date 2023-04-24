7ffd0bfad735759663ee33bdaa2d2ec6e8177c3e
jijiwei <jijiwei@lilianinfo.com>
2023-04-24 18:27:42 +0800

    hook父组件调用子组件的方法

--- src/components/Child/index.jsx
+++ src/components/Child/index.jsx
@@ -1,18 +1,18 @@
-import { useState, useImperativeHandle, forwardRef } from "react";
-import React from "react";
+import React, { useState, useImperativeHandle, forwardRef } from "react";
 function Child(props, ref) {
   const [num, setNum] = useState(0);
   function addNum() {
     setNum((num) => {
-      return num + 1
+      return num + 1;
     });
   }
   useImperativeHandle(ref, () => {
     return {
+      // 此处给父组件暴露方法
       addOne: addNum,
     };
   });
-  
+
   return <div>{num}</div>;
 }
 export default forwardRef(Child);
--- src/pages/vDom.jsx
+++ src/pages/vDom.jsx
@@ -4,7 +4,6 @@ import Child from "@/components/Child/index.jsx";
 export default function VDom() {
   const refChild = useRef(null)
   function addOne () {
-    console.log("refChild", refChild.current)
     refChild.current.addOne()
   }
   return (
