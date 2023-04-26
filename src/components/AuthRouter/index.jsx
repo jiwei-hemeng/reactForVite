2eebcabe3bc3edd191da7cbc4025e5e08b597cb9
jijiwei <jijiwei@lilianinfo.com>
2023-04-26 09:51:09 +0800

    性能的优化

--- src/components/AuthRouter/index.jsx
+++ src/components/AuthRouter/index.jsx
@@ -1,31 +1,21 @@
 // @ts-nocheck
 import React from "react";
 import { connect } from "react-redux";
-import { useMemo, Suspense } from "react";
+import { useMemo } from "react";
 import { Navigate } from "react-router-dom";
-import Loadding from "@/components/loadding";
 function AuthRouter(props) {
-  if(props.title) {
-    document.title = props.title
+  if (props.title) {
+    document.title = props.title;
   }
   const isLogin = useMemo(() => !!props.token, [props.token]);
-  return (
-    <Suspense fallback={<Loadding />}>
-      {!props.auth || (props.auth && isLogin) ? (
-        <props.element />
-      ) : (
-        <Navigate to={`/login?url=/index/${props.path}`} />
-      )}
-    </Suspense>
-  );
+  if (!props.auth || (props.auth && isLogin)) {
+    return <props.element />;
+  }
+  return <Navigate to={`/login?url=/index/${props.path}`} />;
 }
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
   return {
     token: state.token,
   };
 };
-// 操作共享的数据
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {};
-};
-export default connect(mapStateToProps, mapDispatchToProps)(AuthRouter);
+export default connect(mapStateToProps)(AuthRouter);
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 // @ts-nocheck
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@/store/index.js";
 import App from "./App";
+import Loadding from "@/components/loadding";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
     <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <Suspense fallback={<Loadding />}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Suspense>
     </Provider>
   </HashRouter>
 );
