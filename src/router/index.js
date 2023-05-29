e5428e371edf8343287f8bf53b7bee2e13a22edf
jijiwei <jijiwei@lilianinfo.com>
2023-05-29 17:59:39 +0800

    分享Api

--- src/router/index.js
+++ src/router/index.js
@@ -4,4 +4,5 @@ export const router = {
   invoices: lazy(() => import("@/pages/invoices.jsx")),
   vDom: lazy(() => import("@/pages/vDom.jsx")),
   Indexdb: lazy(() => import("@/pages/indexdb.jsx")),
+  share: lazy(() => import("@/pages/share.jsx")),
 };
