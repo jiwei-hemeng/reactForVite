427df6b5d77944162ff5f3e2977acf12033998fe
jiwei-hemeng <jiwei7065@sina.com>
2025-02-28 12:37:35 +0800

    localforage

--- src/pages/indexdb.jsx
+++ src/pages/indexdb.jsx
@@ -1,26 +1,35 @@
 // @ts-nocheck
 import React, { useRef, useEffect } from "react";
-import indexdbHelper from "@/utils/Indexdb.js";
+import { db } from "@/utils/localforage";
+import styles from "./indexdb.module.css";
 export default function Indexdb() {
   async function getData() {
-    const routers = await indexdbHelper.readAll("routers");
-    console.log("routers", routers);
+    const routers = await db.getItem("routers");
+    const keys = await db.keys();
+    console.log("routers", routers, keys);
   }
   async function addData() {
-    await indexdbHelper.save(
-      {
-        // id: Date.now(),
-        name: "name" + parseInt(Math.random() * 100),
-        path: "path" + parseInt(Math.random() * 100),
-        moduleType: Math.round(Math.random())
-      },
-      "routers"
-    );
+    await db.setItem("routerss", {
+      // id: Date.now(),
+      name: "name" + parseInt(Math.random() * 100),
+      path: "path" + parseInt(Math.random() * 100),
+      moduleType: Math.round(Math.random()),
+    });
+  }
+  async function deleteData() {
+    await db.removeItem("routerss");
   }
   return (
-    <div>
-      <button onClick={addData}>添加数据</button>
-      <button onClick={getData}>查询数据</button>
+    <div className={styles.buttonGroup}>
+      <button className={styles.buttonItem} onClick={addData}>
+        添加数据
+      </button>
+      <button className={styles.buttonItem} onClick={getData}>
+        查询数据
+      </button>
+      <button className={styles.buttonItem} onClick={deleteData}>
+        删除数据
+      </button>
     </div>
   );
 }
